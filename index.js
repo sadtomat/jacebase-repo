a6be7dec46f554102a0bd4d480b2d06bcd3559c6
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-02-06 15:10:16 -0500

    added log to test output

--- index.js
+++ index.js
@@ -482,7 +482,7 @@ app.get("/charts", (req, res) => {
     async function pullGameOpponents(id){
       try {
         const response1 = await fetch("/api/"+id+"/game-opponents");
-        console.log(response1);
+        console.log(response1.json());
         return await response1.json();
       } catch (err) {
         console.error(err);
