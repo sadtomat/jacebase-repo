15a6b200e30bb4d6df655e1e3f5c7f8c59903c96
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-02-07 13:31:21 -0500

    bug fixing

--- index.js
+++ index.js
@@ -500,28 +500,20 @@ app.get("/charts", (req, res) => {
         deckElo = 0
         playingInstances = instanceTable.filter(obj => obj.DeckName === deck.name)
         for (instance of playingInstances) {
-          pullGameOpponents(instance.instanceID).then((opponents) => {
-            eloGain = 10;
-            for (opp of opponents) {
-              eloFetch = {name: "", elo: 4.3};
-              eloFetch = rawDeckElo.find(obj => obj.name === opp.DeckName);
-              //console.log(opponents);
+          const gameOpponents = await pullGameOpponents(instance.instanceID);
+          eloGain = 10;
+          for (opponents of gameOpponents) {
+            eloFetch = rawDeckElo.find(obj => obj.name === opponents.DeckName);
+            if (eloFetch){
               console.log(typeof eloFetch.elo);
               let eloVal = eloFetch.elo;
               eloGain = eloGain + eloVal;
+              console.log("FOUND: "+eloFetch.name);
+            }else{
+              console.warn("NOT FOUND");
             }
-            deckElo = deckElo + eloGain
-          });
-          //eloGain = 10;
-          //for (opponents of gameOpponents) {
-          //  eloFetch = {name: "", elo: 4.3};
-          //  eloFetch = rawDeckElo.find(obj => obj.name === opponents.DeckName);
-            //console.log(opponents);
-          //  console.log(typeof eloFetch.elo);
-          //  let eloVal = eloFetch.elo;
-          //  eloGain = eloGain + eloVal;
-          //}
-          //deckElo = deckElo + eloGain
+          }
+          deckElo = deckElo + eloGain
         }
         console.log("Deck: "+deck.DeckName+", Elo:"+deckElo)
 
