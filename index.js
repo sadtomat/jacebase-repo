48428f85d7d043ca6ef80cff135a1c213b131a59
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-02-11 17:45:16 -0500

    fixed bug

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.get("/", (req, res) => {
       username = document.getElementById("uname").value;
       password = document.getElementById("pword").value;
       const response = await fetch('/api/set-login', {
-        method: 'POST',
+        method: 'GET',
         headers: {
           'Content-Type': 'application/json'
         },
