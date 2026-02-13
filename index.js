c48a206e999e8655feb5de70e93b9af176dca1b3
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-02-13 14:43:55 -0500

    minor styling changes

--- index.js
+++ index.js
@@ -1098,7 +1098,7 @@ app.get("/home", (req, res) => {
         height: 40px;
         margin-top: 5px;
         border: 4px outset;
-        width: 120px;
+        width: 150px;
       }
       .playerbox select {
         border: 4px inset;
