ccfb42a28c025e7cf2be77750484fd27162dc31f
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-06 18:29:54 -0400

    adding babel

--- Pages/index.jsx
+++
@@ -1,6 +0,0 @@
-//import React from 'react';
-import { React } from 'react';
-import { ReactDOM } from 'react-dom';
-import { login } from './login.jsx';
-
-ReactDOM.render(<login />, document.getElementById('root'));
