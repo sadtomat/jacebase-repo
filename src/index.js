737d24efad3ee19c8af672ba2c46ff309d898e7f
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-06 18:53:28 -0400

    test page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import tester from './tester';
+import Tester from './tester';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <tester />
+    <Tester />
   </div>
 );
 
