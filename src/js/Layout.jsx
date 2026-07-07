682a6f904b75b552652da2c1012d2acf31378861
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-07 15:43:32 -0400

    background addition test

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -1,11 +1,24 @@
 import { Navbar } from "./Navbar"
 import { Outlet } from "react-router-dom"
+// const { Client } = require("pg");
+
+// const dbConfig = {
+//     host: process.env.DB_HOST || "localhost",
+//     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+//     ssl: (process.env.DB_HOST && process.env.DB_HOST != 'localhost') ? { ca: fs.readFileSync('global-bundle.pem').toString() } : false,
+//     user: process.env.DB_USER || "postgres",
+//     password: process.env.DB_PASS || "postgres",
+//     database: "jacebase-db" // Connect to the default database
+// };
+
 
 export function Layout() {
+    // const client = new Client(dbConfig);
+    // await client.connect;
     return (
         <>
             <Navbar/>
-            <main>
+            <main class="container">
                 <Outlet/>
             </main>
         </>
