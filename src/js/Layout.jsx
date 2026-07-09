679276375770a84eed36d96d3a7e8cf4f909b35b
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-09 13:03:00 -0400

    added login.css

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -19,7 +19,7 @@ export function Layout() {
     return (
         <>
             <Navbar/>
-            <main>
+            <main className="container">
                 <Outlet/>
             </main>
         </>
