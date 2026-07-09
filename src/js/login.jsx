9a17c04b6750c7d970bca9371c8328dfe52b6619
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-09 16:05:19 -0400

    added loginform.jsx, integrating

--- src/js/login.jsx
+++ src/js/login.jsx
@@ -1,4 +1,5 @@
 import '../css/Login.css'
+import './LoginForm'
 
 export function Login() {
     return (
@@ -7,14 +8,7 @@ export function Login() {
                 <img className="imgbox" src={`https://${process.env.REACT_APP_S3_BUCKET}.s3.${process.env.REACT_APP_S3_REGION}.amazonaws.com/Jacebase-login.jpg`}/>
                 <div className="smallbox">
                     <p className="entertext">Enter username and password</p>
-                    <form className="upform">
-                        <label>Username:</label>
-                        <input></input>
-                    </form>
-                    <form className="upform">
-                        <label>Password:</label>
-                        <input></input>
-                    </form>
+                    <LoginForm/>
                     <div>
                         <button className="exitbutton">Submit</button>
                         <button className="exitbutton">Login as Guest</button>
