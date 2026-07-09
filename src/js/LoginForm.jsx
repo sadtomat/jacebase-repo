3b845f8d321964877dc883fdd9b3362c6d0d838e
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-09 16:25:13 -0400

    loginform rework

--- src/js/LoginForm.jsx
+++ src/js/LoginForm.jsx
@@ -1,33 +1,22 @@
 import React from 'react';
 
-class LoginForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {value: ''};
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        alert('Username: ' + this.state.value);
+export function LoginForm() {
+    function handleSubmit(event) {
+        const form = event.target;
+        const formData = new FormData(form);
+        const username = formData.get('username');
+        alert('Username: ' + username);
         event.preventDefault();
     }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Username:
-                    <input type='text' value={this.state.value} onChange={this.handleChange}/>
-                </label>
-                <input type='submit' value='Submit'/>
-            </form>
-        );
-    }
+    return (
+        <form onSubmit={this.handleSubmit}>
+            <label>
+                Username:
+                <input type='text' name='username'/>
+            </label>
+            <button type='submit'/>
+        </form>
+    );
 
 }
