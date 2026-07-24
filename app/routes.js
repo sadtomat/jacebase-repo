9e7d129082cbf251eba08361c26e4a683d409489
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-24 16:35:22 -0400

    router logic typo

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ import {route, index, layout} from "@react-router/dev/routes";
 
 export default [
   layout("./routes/layout.jsx", [
-    index("/", "./routes/login.jsx"),
+    route("/", "./routes/login.jsx"),
     route("deck-entry", "./routes/deck-entry.jsx"),
     route("game-entry", "./routes/game-entry.jsx"),
     route("statistics", "./routes/statistics.jsx"),
