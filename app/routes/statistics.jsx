c49faee7e4e0a70d5d79556fd97d172897d73d71
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-24 16:27:48 -0400

    adding loader to statistics

--- app/routes/statistics.jsx
+++ app/routes/statistics.jsx
@@ -1,4 +1,19 @@
+import { data, redirect } from "react-router";
+import { getSession } from "../services/session.server.js";
+
+export async function loader(request){
+    const session = await getSession(request.headers.get("Cookie"));
+    const user = session.get("user");
+    if (!user) {
+        console.log("no login");
+    }else{
+        console.log("yes login");
+    }
+    return data({user});
+}
+
 export function Statistics() {
+
     return (
         <>
             <h1>This is Statistics Page</h1>
