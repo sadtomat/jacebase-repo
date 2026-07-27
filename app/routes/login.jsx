275329d1881413b164c9575830b5e6b7eab6132c
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-27 15:43:49 -0400

    minor typo fix

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -3,7 +3,7 @@ import {LoginForm} from '../js/LoginForm'
 import { getSession } from "../services/session.server.js";
 
 export async function loader(request){
-    const session = await getSession(request.headers.get("Cookie"));
+    const session = await getSession(request.headers("Cookie"));
     console.log("session: ", session);
     // const user = session.get("user");
     // if (!user) {
