5d5a5e4c19b48a6512e8b9c1c99b30449cf3c4c8
sadtomat1999 <zachariah.friesen@ontariotechu.net>
2026-07-27 13:52:55 -0400

    console log test

--- app/routes/game-entry.jsx
+++ app/routes/game-entry.jsx
@@ -3,13 +3,15 @@ import { getSession } from "../services/session.server.js";
 
 export async function loader(request){
     const session = await getSession(request.headers.get("Cookie"));
-    const user = session.get("user");
-    if (!user) {
-        console.log("no login");
-    }else{
-        console.log("yes login");
-    }
-    return data({user});
+    console.log(session);
+    return;
+    // const user = session.get("user");
+    // if (!user) {
+    //     console.log("no login");
+    // }else{
+    //     console.log("yes login");
+    // }
+    // return data({user});
 }
 
 export function GameEntry() {
